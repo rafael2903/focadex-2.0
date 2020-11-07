81efc0746eb34c916f50996f98ba33d59988be70
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2020-11-07 12:03:49 -0300

    add pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import P1 from './components/p1'
-import P2 from './components/p2'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <P1 />
-        <P2 />
-        
-      </header>
+  
     </div>
   );
 }
--- src/components/p1.js
+++
@@ -1,9 +0,0 @@
-function P1() {
-  return (
-    <div className="P1">
-      <p>BNrafa</p>
-    </div>
-  );
-}
-
-export default P1;
