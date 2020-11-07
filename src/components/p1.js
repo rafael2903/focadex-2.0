2961d81afa0c0b2db187604e2aee88d1ab3cbce2
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2020-11-07 10:02:07 -0300

    ddd

---
+++ src/components/p1.js
@@ -0,0 +1,9 @@
+function P1() {
+  return (
+    <div className="P1">
+      <p>BN</p>
+    </div>
+  );
+}
+
+export default P1;
---
+++ src/components/p2.js
@@ -0,0 +1,9 @@
+function P2() {
+    return (
+      <div className="P2">
+        <p>GHJ</p>
+      </div>
+    );
+  }
+  
+  export default P2;
