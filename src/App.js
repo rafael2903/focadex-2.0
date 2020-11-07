686134262a6758b97e7abb6902a2bfcb8138bdab
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2020-11-07 13:36:01 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, {useState} from "react";
+import Home from "./pages/Home"
+import LoginPage from "./pages/LoginPage";
+import PageNotFound from "./pages/PageNotFound";
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
+
+  const [loggedIn,setLogin] = useState(() => {
+    return JSON.parse(localStorage.getItem('logged'));
+  });
+
+  
   return (
     <div className="App">
-  
+      <Router>
+          <Switch>
+
+            <Route exact path="/">
+              {loggedIn ? <Home setLogin={setLogin}/> : <Redirect to="/login" />}
+            </Route>
+
+            <Route path="/login">
+              {loggedIn ? <Redirect to="/" /> : <LoginPage  setLogin={setLogin} />}
+            </Route>
+
+            <Route path="">
+              <PageNotFound/>
+            </Route>
+            
+          </Switch>
+        </Router>
     </div>
   );
 }
